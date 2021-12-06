46adcf0863d0ed8639fd6c4cdd2ec39472c1f56e
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-06 21:45:01 +0100

    started

--- api/server.js
+++ api/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
-
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it
+const server = require('./api/server')
+
+const PORT = 5000
+
+server.listen(PORT, (req,res) => {
+   console.log('listening on', PORT)
+})
+
+module.exports = server;
