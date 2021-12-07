43dbb4a12c3a690c6b0685389b0c08a33bc29ca6
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-07 03:29:18 +0100

    done with logger

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // require your server and launch it
 const server = require('./api/server')
-
+const { logger } = require('./api/middleware/middleware')
 const PORT = 5000
+server.use(logger)
 
 server.listen(PORT, (req,res) => {
    console.log('listening on', PORT)
