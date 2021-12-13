88f2ff6e2a432ad5543781110936470455fcdcd6
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-13 10:18:47 +0100

    Posts are now working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -30,7 +30,7 @@ async function validateUserId(req, res, next) {
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
   const { name } = req.body;
-  if (!name || name.trim()) {
+  if (!name || !name.trim()) {
     res.status(400).json({
       message: "missing required name field",
     });
